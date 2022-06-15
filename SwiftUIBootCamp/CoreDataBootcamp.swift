84e8ad2f400391e6887f9c08538a8f7f40a7aa97
MickhailP <mich_p@inbox.ru>
2022-06-15 21:20:46 +0300

    First commit

---
+++ SwiftUIBootCamp/CoreDataBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataBootcamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Миша Перевозчиков on 15.06.2022.
+//
+
+import SwiftUI
+
+struct CoreDataBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoreDataBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        CoreDataBootcamp()
+    }
+}
