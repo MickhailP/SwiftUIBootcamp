0e2d8676577e756e63b2ddebc85492602b9f9285
MickhailP <mich_p@inbox.ru>
2022-06-23 22:41:00 +0300

    More  features

---
+++ SwiftUIBootCamp/DownloadWithEscaping.swift
@@ -0,0 +1,27 @@
+//
+//  DownloadWithEscaping.swift
+//  SwiftUIBootCamp
+//
+//  Created by Миша Перевозчиков on 23.06.2022.
+//
+
+import SwiftUI
+
+
+class DownloadWithEscapingViewModel: ObservableObject {
+     
+}
+
+struct DownloadWithEscaping: View {
+    
+    @
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DownloadWithEscaping_Previews: PreviewProvider {
+    static var previews: some View {
+        DownloadWithEscaping()
+    }
+}
